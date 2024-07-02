4a09bcb30e581e83a1fee8a05a47b7da342a3646
LoneWolf <ibbi014@outlook.com>
2024-07-02 13:03:56 +0500

    Added Init Function

--- src/ringbuf.c
+++ src/ringbuf.c
@@ -1,4 +1,5 @@
 #include "../include/ringbuf.h"
+#include <pthread.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -7,7 +8,16 @@
 
 void ringbuffer_init(rbctx_t *context, void *buffer_location, size_t buffer_size)
 {
-    /* your solution here */
+  context->begin = (uint8_t *)buffer_location;
+  context->end = (uint8_t *)buffer_location + buffer_size;
+  context->read = context->begin; 
+  context->write = context->begin;
+
+  pthread_mutex_init(&context->mutex_read, NULL);
+  pthread_mutex_init(&context->mutex_write, NULL);
+ 
+  pthread_cond_init(&context->signal_read, NULL);
+  pthread_cond_init(&context->signal_write, NULL);
 }
 
 int ringbuffer_write(rbctx_t *context, void *message, size_t message_len)
