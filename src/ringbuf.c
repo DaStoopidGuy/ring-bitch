b4ace801542d9ba7cf66ebf04e7e5efd9f7ff874
LoneWolf <ibbi014@outlook.com>
2024-07-02 16:29:19 +0500

    Task 1 Completed - All tests passed

--- src/ringbuf.c
+++ src/ringbuf.c
@@ -24,75 +24,75 @@ int ringbuffer_write(rbctx_t *context, void *message, size_t message_len) {
   pthread_mutex_lock(&context->mutex_write);
 
   size_t total_space = context->end - context->begin;
-  size_t available_space = context->end - context->write;
   size_t space_needed = message_len + sizeof(size_t);
 
-  if (space_needed > total_space)
+  if (space_needed > total_space) {
+    pthread_mutex_unlock(&context->mutex_write);
     return RINGBUFFER_FULL;
-
-  // check if there is Enough space before the end of the buffer
-  if (available_space >= space_needed) {
-    // write the length of the message
-    size_t *len_ptr = (size_t *)context->write;
-    *len_ptr = message_len;
-    context->write += sizeof(message_len);
-    // write message data
-    memcpy(context->write, message, message_len);
-    context->write += message_len; // move write pointer to after the message
-
-    if (context->write == context->end)
-      context->write = context->begin;
-    // signal that data has been written
-    pthread_cond_signal(&context->signal_write);
-  } else {
-    // if the the available space less than the space we need to write
-    // write message length
-    if (available_space < sizeof(message_len)) {
-      size_t first_chunk_size = available_space;
-      size_t second_chunk_size = sizeof(message_len) - first_chunk_size;
-
-      memcpy(context->write, &message_len, first_chunk_size);
-      context->write = context->begin + second_chunk_size;
-    } else {
-      size_t *len_ptr = (size_t *)context->write;
-      *len_ptr = message_len;
-      context->write += message_len;
-    }
-
-    // recalculate space available
-    available_space = context->end - context->write;
-
-    if (available_space < message_len) {
-
-      // write message data
-      size_t first_chunk_size = available_space;
-      size_t second_chunk_size = space_needed - available_space;
-
-      // TODO: do not overtake read ptr, check this shit please
-      memcpy(context->write, message, first_chunk_size);
-      memcpy(context->begin, message + first_chunk_size, second_chunk_size);
-
-      context->write = context->begin + second_chunk_size;
-    } else {
-      // write message data
-      memcpy(context->write, message, message_len);
-      context->write += message_len; // move write pointer to after the message
-    }
-    // signal that data has been written
-    pthread_cond_signal(&context->signal_write);
   }
 
+  size_t available_space;
+  if (context->write >= context->read) {
+    available_space =
+        (context->end - context->write) + (context->read - context->begin) - 1;
+  } else {
+    available_space = context->read - context->write - 1;
+  }
+
+  while (available_space < space_needed) {
+    pthread_cond_wait(&context->signal_write, &context->mutex_write);
+
+    if (context->write >= context->read) {
+      available_space = (context->end - context->write) +
+                        (context->read - context->begin) - 1;
+    } else {
+      available_space = context->read - context->write - 1;
+    }
+  }
+
+  if (context->write + sizeof(size_t) <= context->end) {
+    memcpy(context->write, &message_len, sizeof(size_t));
+    context->write += sizeof(size_t);
+
+    if (context->write == context->end) {
+      context->write = context->begin;
+    }
+  } else {
+    size_t first_part_size = context->end - context->write;
+    memcpy(context->write, &message_len, first_part_size);
+    memcpy(context->begin, (uint8_t *)&message_len + first_part_size,
+           sizeof(size_t) - first_part_size);
+    context->write = context->begin + (sizeof(size_t) - first_part_size);
+  }
+
+  if (context->write + message_len <= context->end) {
+    memcpy(context->write, message, message_len);
+    context->write += message_len;
+
+    if (context->write == context->end) {
+      context->write = context->begin;
+    }
+  } else {
+    size_t first_part_size = context->end - context->write;
+    memcpy(context->write, message, first_part_size);
+    memcpy(context->begin, (uint8_t *)message + first_part_size,
+           message_len - first_part_size);
+    context->write = context->begin + (message_len - first_part_size);
+  }
+
+  pthread_cond_signal(&context->signal_read);
+
   pthread_mutex_unlock(&context->mutex_write);
-  return SUCCESS;
+  return 0;
 }
 
 size_t read_message_size_from_ringbuffer(rbctx_t *context) {
-  size_t size;
+  size_t value;
   uint8_t *read_ptr = context->read;
   size_t size_t_size = sizeof(size_t);
 
   if (read_ptr + size_t_size <= context->end) {
-    memcpy(&size, read_ptr, size_t_size);
+    memcpy(&value, read_ptr, size_t_size);
   } else {
     size_t first_part_size = context->end - read_ptr;
     uint8_t temp_buffer[size_t_size];
@@ -100,15 +100,15 @@ size_t read_message_size_from_ringbuffer(rbctx_t *context) {
     memcpy(temp_buffer, read_ptr, first_part_size);
     memcpy(temp_buffer + first_part_size, context->begin,
            size_t_size - first_part_size);
-
-    memcpy(&size, temp_buffer, size_t_size);
+    memcpy(&value, temp_buffer, size_t_size);
   }
+
   context->read += size_t_size;
   if (context->read >= context->end) {
     context->read = context->begin + (context->read - context->end);
   }
 
-  return size;
+  return value;
 }
 
 int ringbuffer_read(rbctx_t *context, void *buffer, size_t *buffer_len) {
@@ -116,9 +116,17 @@ int ringbuffer_read(rbctx_t *context, void *buffer, size_t *buffer_len) {
   while (context->read == context->write) {
     pthread_cond_wait(&context->signal_read, &context->mutex_read);
   }
+
   size_t size = read_message_size_from_ringbuffer(context);
+
+  // Check if the provided buffer is large enough
+  if (*buffer_len < size) {
+    pthread_mutex_unlock(&context->mutex_read);
+    return -1; // Buffer too small
+  }
   if (context->read + size <= context->end) {
     memcpy(buffer, context->read, size);
+    context->read += size;
   } else {
     size_t first_part = context->end - context->read;
     size_t second_part = size - first_part;
@@ -129,7 +137,6 @@ int ringbuffer_read(rbctx_t *context, void *buffer, size_t *buffer_len) {
   if (context->read == context->end) {
     context->read = context->begin;
   }
-
   pthread_cond_signal(&context->signal_write);
   pthread_mutex_unlock(&context->mutex_read);
   *buffer_len = size;
