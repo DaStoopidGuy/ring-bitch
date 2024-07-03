3563d3a4981fe24e5c45587c21f0675455a2d5d1
LoneWolf <ibbi014@outlook.com>
2024-07-03 12:44:22 +0500

    Task 3 Logic Implented - Test Fails, Possible Issue, with the way data is read

--- src/daemon.c
+++ src/daemon.c
@@ -1,188 +1,262 @@
-#include <stdio.h>
 #include <pthread.h>
-#include <unistd.h>
-#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "../include/daemon.h"
 #include "../include/ringbuf.h"
 
-/* IN THE FOLLOWING IS THE CODE PROVIDED FOR YOU 
+/* IN THE FOLLOWING IS THE CODE PROVIDED FOR YOU
  * changing the code will result in points deduction */
 
 /********************************************************************
-* NETWORK TRAFFIC SIMULATION: 
-* This section simulates incoming messages from various ports using 
-* files. Think of these input files as data sent by clients over the
-* network to our computer. The data isn't transmitted in a single 
-* large file but arrives in multiple small packets. This concept
-* is discussed in more detail in the advanced module: 
-* Rechnernetze und Verteilte Systeme
-*
-* To simulate this parallel packet-based data transmission, we use multiple 
-* threads. Each thread reads small segments of the files and writes these 
-* smaller packets into the ring buffer. Between each packet, the
-* thread sleeps for a random time between 1 and 100 us. This sleep
-* simulates that data packets take varying amounts of time to arrive.
-*********************************************************************/
+ * NETWORK TRAFFIC SIMULATION:
+ * This section simulates incoming messages from various ports using
+ * files. Think of these input files as data sent by clients over the
+ * network to our computer. The data isn't transmitted in a single
+ * large file but arrives in multiple small packets. This concept
+ * is discussed in more detail in the advanced module:
+ * Rechnernetze und Verteilte Systeme
+ *
+ * To simulate this parallel packet-based data transmission, we use multiple
+ * threads. Each thread reads small segments of the files and writes these
+ * smaller packets into the ring buffer. Between each packet, the
+ * thread sleeps for a random time between 1 and 100 us. This sleep
+ * simulates that data packets take varying amounts of time to arrive.
+ *********************************************************************/
 typedef struct {
-    rbctx_t* ctx;
-    connection_t* connection;
+  rbctx_t *ctx;
+  connection_t *connection;
 } w_thread_args_t;
 
-void* write_packets(void* arg) {
-    /* extract arguments */
-    rbctx_t* ctx = ((w_thread_args_t*) arg)->ctx;
-    size_t from = (size_t) ((w_thread_args_t*) arg)->connection->from;
-    size_t to = (size_t) ((w_thread_args_t*) arg)->connection->to;
-    char* filename = ((w_thread_args_t*) arg)->connection->filename;
+pthread_mutex_t ring_buffer_mutex;
+pthread_cond_t ring_buffer_cond = PTHREAD_COND_INITIALIZER;
 
-    /* open file */
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Cannot open file with name %s\n", filename);
-        exit(1);
-    }
+void *write_packets(void *arg) {
+  /* extract arguments */
+  rbctx_t *ctx = ((w_thread_args_t *)arg)->ctx;
+  size_t from = (size_t)((w_thread_args_t *)arg)->connection->from;
+  size_t to = (size_t)((w_thread_args_t *)arg)->connection->to;
+  char *filename = ((w_thread_args_t *)arg)->connection->filename;
 
-    /* read file in chunks and write to ringbuffer with random delay */
-    unsigned char buf[MESSAGE_SIZE];
-    size_t packet_id = 0;
-    size_t read = 1;
-    while (read > 0) {
-        size_t msg_size = MESSAGE_SIZE - 3 * sizeof(size_t);
-        read = fread(buf + 3 * sizeof(size_t), 1, msg_size, fp);
-        if (read > 0) {
-            memcpy(buf, &from, sizeof(size_t));
-            memcpy(buf + sizeof(size_t), &to, sizeof(size_t));
-            memcpy(buf + 2 * sizeof(size_t), &packet_id, sizeof(size_t));
-            while(ringbuffer_write(ctx, buf, read + 3 * sizeof(size_t)) != SUCCESS){
-                usleep(((rand() % 50) + 25)); // sleep for a random time between 25 and 75 us
-            }
-        }
-        packet_id++;
-        usleep(((rand() % (100 -1)) + 1)); // sleep for a random time between 1 and 100 us
+  /* open file */
+  FILE *fp = fopen(filename, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Cannot open file with name %s\n", filename);
+    exit(1);
+  }
+
+  /* read file in chunks and write to ringbuffer with random delay */
+  unsigned char buf[MESSAGE_SIZE];
+  size_t packet_id = 0;
+  size_t read = 1;
+  while (read > 0) {
+    size_t msg_size = MESSAGE_SIZE - 3 * sizeof(size_t);
+    read = fread(buf + 3 * sizeof(size_t), 1, msg_size, fp);
+    if (read > 0) {
+      memcpy(buf, &from, sizeof(size_t));
+      memcpy(buf + sizeof(size_t), &to, sizeof(size_t));
+      memcpy(buf + 2 * sizeof(size_t), &packet_id, sizeof(size_t));
+      while (ringbuffer_write(ctx, buf, read + 3 * sizeof(size_t)) != SUCCESS) {
+        usleep(((rand() % 50) +
+                25)); // sleep for a random time between 25 and 75 us
+      }
     }
-    fclose(fp);
-    return NULL;
+    packet_id++;
+    usleep(((rand() % (100 - 1)) +
+            1)); // sleep for a random time between 1 and 100 us
+  }
+  fclose(fp);
+  return NULL;
 }
-
-/* END OF PROVIDED CODE */
-
 
 /********************************************************************/
 
-/* YOUR CODE STARTS HERE */
+pthread_mutex_t file_mutexes[MAXIMUM_PORT];
+char *destination_file_names[MAXIMUM_PORT];
+// Filtering function
+int should_filter(size_t from_port, size_t to_port, char *message_content) {
+  if (from_port == to_port || from_port == 42 || to_port == 42 ||
+      (from_port + to_port) == 42 ||
+      strstr(message_content, "malicious") != NULL) {
+    return 1; // Message should be filtered out
+  }
+  return 0; // Message should be processed
+}
 
-// 1. read functionality
-// 2. filtering functionality
-// 3. (thread-safe) write to file functionality
+// File writing function
+void write_to_file(size_t port, const char *message_content,
+                   const size_t message_len) {
+  // Thread-safe write to file using mutex
+  pthread_mutex_lock(&file_mutexes[port]);
+  FILE *fp = fopen(destination_file_names[port], "a");
+  if (fp != NULL) {
+    fwrite(message_content, 1, message_len, fp);
+    fclose(fp);
+  }
+  pthread_mutex_unlock(&file_mutexes[port]);
+}
+
+void *read_packets(void *arg) {
+  rbctx_t *ctx = (rbctx_t *)arg;
+  pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+  pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+  while (1) {
+    pthread_testcancel();
+    unsigned char buf[MESSAGE_SIZE];
+    size_t read_len = MESSAGE_SIZE;
+
+    pthread_mutex_lock(&ring_buffer_mutex);
+    int result = ringbuffer_read(ctx, buf, &read_len);
+    pthread_mutex_unlock(&ring_buffer_mutex);
+    if (result == RINGBUFFER_EMPTY) {
+      continue;
+    } else if (result == -1) {
+      // Handle buffer too small condition
+      continue;
+    }
+
+    // Extract metadata from the packet
+    size_t from_port, to_port, packet_id;
+    memcpy(&from_port, buf, sizeof(size_t));
+    memcpy(&to_port, buf + sizeof(size_t), sizeof(size_t));
+    memcpy(&packet_id, buf + 2 * sizeof(size_t), sizeof(size_t));
+
+    // Extract message content
+    char *message_content = (char *)(buf + 3 * sizeof(size_t));
+    size_t message_len = read_len - 3 * sizeof(size_t);
+    printf("%s", message_content);
+    printf("\n");
+    // Implement filtering
+    if (should_filter(from_port, to_port, message_content)) {
+      // Discard message based on filtering criteria
+      continue;
+    }
+
+    // Write message content to destination file
+    write_to_file(to_port, message_content, message_len);
+  }
+
+  return NULL;
+}
 
 /* YOUR CODE ENDS HERE */
 
 /********************************************************************/
 
-int simpledaemon(connection_t* connections, int nr_of_connections) {
-    /* initialize ringbuffer */
-    rbctx_t rb_ctx;
-    size_t rbuf_size = 1024;
-    void *rbuf = malloc(rbuf_size);
-    if (rbuf == NULL) {
-        fprintf(stderr, "Error allocation ringbuffer\n");
+int simpledaemon(connection_t *connections, int nr_of_connections) {
+  /* initialize ringbuffer */
+  rbctx_t rb_ctx;
+  size_t rbuf_size = 1024;
+  void *rbuf = malloc(rbuf_size);
+  if (rbuf == NULL) {
+    fprintf(stderr, "Error allocation ringbuffer\n");
+  }
+
+  ringbuffer_init(&rb_ctx, rbuf, rbuf_size);
+
+  /****************************************************************
+   * WRITER THREADS
+   * ***************************************************************/
+
+  /* prepare writer thread arguments */
+  w_thread_args_t w_thread_args[nr_of_connections];
+  for (int i = 0; i < nr_of_connections; i++) {
+    w_thread_args[i].ctx = &rb_ctx;
+    w_thread_args[i].connection = &connections[i];
+    /* guarantee that port numbers range from MINIMUM_PORT (0) - MAXIMUMPORT */
+    if (connections[i].from > MAXIMUM_PORT ||
+        connections[i].to > MAXIMUM_PORT ||
+        connections[i].from < MINIMUM_PORT ||
+        connections[i].to < MINIMUM_PORT) {
+      fprintf(stderr, "Port numbers %d and/or %d are too large\n",
+              connections[i].from, connections[i].to);
+      exit(1);
     }
+  }
 
-    ringbuffer_init(&rb_ctx, rbuf, rbuf_size);
+  /* start writer threads */
+  pthread_t w_threads[nr_of_connections];
+  for (int i = 0; i < nr_of_connections; i++) {
+    pthread_create(&w_threads[i], NULL, write_packets, &w_thread_args[i]);
+  }
 
-    /****************************************************************
-    * WRITER THREADS 
-    * ***************************************************************/
+  /****************************************************************
+   * READER THREADS
+   * ***************************************************************/
 
-    /* prepare writer thread arguments */
-    w_thread_args_t w_thread_args[nr_of_connections];
-    for (int i = 0; i < nr_of_connections; i++) {
-        w_thread_args[i].ctx = &rb_ctx;
-        w_thread_args[i].connection = &connections[i];
-        /* guarantee that port numbers range from MINIMUM_PORT (0) - MAXIMUMPORT */
-        if (connections[i].from > MAXIMUM_PORT || connections[i].to > MAXIMUM_PORT ||
-            connections[i].from < MINIMUM_PORT || connections[i].to < MINIMUM_PORT) {
-            fprintf(stderr, "Port numbers %d and/or %d are too large\n", connections[i].from, connections[i].to);
-            exit(1);
-        }
-    }
+  pthread_t r_threads[NUMBER_OF_PROCESSING_THREADS];
 
-    /* start writer threads */
-    pthread_t w_threads[nr_of_connections];
-    for (int i = 0; i < nr_of_connections; i++) {
-        pthread_create(&w_threads[i], NULL, write_packets, &w_thread_args[i]);
-    }
+  /* END OF PROVIDED CODE */
 
-    /****************************************************************
-    * READER THREADS
-    * ***************************************************************/
+  /********************************************************************/
 
-    pthread_t r_threads[NUMBER_OF_PROCESSING_THREADS];
+  /* YOUR CODE STARTS HERE */
+  for (int i = 0; i < MAXIMUM_PORT; ++i) {
+    pthread_mutex_init(&file_mutexes[i], NULL);
+    destination_file_names[i] = malloc(256);
+    sprintf(destination_file_names[i], "%d.txt", i);
+  }
 
-    /* END OF PROVIDED CODE */
-    
-    /********************************************************************/
+  pthread_mutex_init(&ring_buffer_mutex, NULL);
+  // STARTING READER THREADS
 
-    /* YOUR CODE STARTS HERE */
+  for (int i = 0; i < NUMBER_OF_PROCESSING_THREADS; i++) {
+    pthread_create(&r_threads[i], NULL, read_packets, &rb_ctx);
+  }
 
-    // 1. think about what arguments you need to pass to the processing threads
-    // 2. start the processing threads
+  /* YOUR CODE ENDS HERE */
 
-    /* YOUR CODE ENDS HERE */
+  /********************************************************************/
 
-    /********************************************************************/
+  /* IN THE FOLLOWING IS THE CODE PROVIDED FOR YOU
+   * changing the code will result in points deduction */
 
+  /****************************************************************
+   * CLEANUP
+   * ***************************************************************/
 
+  /* after 5 seconds JOIN all threads (we should definitely have received all
+   * messages by then) */
+  printf("daemon: waiting for 5 seconds before canceling reading threads\n");
+  sleep(5);
+  for (int i = 0; i < NUMBER_OF_PROCESSING_THREADS; i++) {
+    pthread_cancel(r_threads[i]);
+  }
+  /* wait for all threads to finish */
+  for (int i = 0; i < nr_of_connections; i++) {
+    pthread_join(w_threads[i], NULL);
+  }
+  /* join all threads */
+  for (int i = 0; i < NUMBER_OF_PROCESSING_THREADS; i++) {
+    pthread_join(r_threads[i], NULL);
+  }
+  /* END OF PROVIDED CODE */
 
-    /* IN THE FOLLOWING IS THE CODE PROVIDED FOR YOU 
-     * changing the code will result in points deduction */
+  /********************************************************************/
 
-    /****************************************************************
-     * CLEANUP
-     * ***************************************************************/
+  /* YOUR CODE STARTS HERE */
 
-    /* after 5 seconds JOIN all threads (we should definitely have received all messages by then) */
-    printf("daemon: waiting for 5 seconds before canceling reading threads\n");
-    sleep(5);
-    for (int i = 0; i < NUMBER_OF_PROCESSING_THREADS; i++) {
-        pthread_cancel(r_threads[i]);
-    }
+  for (int i = 0; i < MAXIMUM_PORT; ++i) {
+    pthread_mutex_destroy(&file_mutexes[i]);
+    free(destination_file_names[i]);
+  }
 
-    /* wait for all threads to finish */
-    for (int i = 0; i < nr_of_connections; i++) {
-        pthread_join(w_threads[i], NULL);
-    }
+  pthread_mutex_destroy(&ring_buffer_mutex);
+  /* YOUR CODE ENDS HERE */
 
-    /* join all threads */
-    for (int i = 0; i < NUMBER_OF_PROCESSING_THREADS; i++) {
-        pthread_join(r_threads[i], NULL);
-    }
+  /********************************************************************/
 
-    /* END OF PROVIDED CODE */
+  /* IN THE FOLLOWING IS THE CODE PROVIDED FOR YOU
+   * changing the code will result in points deduction */
 
+  free(rbuf);
+  ringbuffer_destroy(&rb_ctx);
 
+  return 0;
 
-    /********************************************************************/
-    
-    /* YOUR CODE STARTS HERE */
-
-    // use this section to free any memory, destory mutexe etc.
-
-    /* YOUR CODE ENDS HERE */
-
-    /********************************************************************/
-
-
-
-    /* IN THE FOLLOWING IS THE CODE PROVIDED FOR YOU 
-    * changing the code will result in points deduction */
-
-    free(rbuf);
-    ringbuffer_destroy(&rb_ctx);
-
-    return 0;
-
-    /* END OF PROVIDED CODE */
+  /* END OF PROVIDED CODE */
 }
--- src/ringbuf.c
+++ src/ringbuf.c
@@ -20,144 +20,149 @@ void ringbuffer_init(rbctx_t *context, void *buffer_location,
   pthread_cond_init(&context->signal_write, NULL);
 }
 
-
 int ringbuffer_write(rbctx_t *context, void *message, size_t message_len) {
-    struct timespec timeout;
-    clock_gettime(CLOCK_REALTIME, &timeout);
-    timeout.tv_sec += 1;
+  struct timespec timeout;
+  clock_gettime(CLOCK_REALTIME, &timeout);
+  timeout.tv_sec += 1;
 
-    pthread_mutex_lock(&context->mutex_write);
+  pthread_mutex_lock(&context->mutex_write);
 
-    size_t total_space = context->end - context->begin;
-    size_t space_needed = message_len + sizeof(size_t);
+  size_t total_space = context->end - context->begin;
+  size_t space_needed = message_len + sizeof(size_t);
 
-    while (1) {
-        if (space_needed > total_space) {
-            pthread_mutex_unlock(&context->mutex_write);
-            return RINGBUFFER_FULL;
-        }
-
-        size_t available_space;
-        if (context->write >= context->read) {
-            available_space = (context->end - context->write) + (context->read - context->begin) - 1;
-        } else {
-            available_space = context->read - context->write - 1;
-        }
-
-        if (available_space >= space_needed) {
-            break;
-        }
-
-        int res = pthread_cond_timedwait(&context->signal_write, &context->mutex_write, &timeout);
-        if (res == ETIMEDOUT) {
-            pthread_mutex_unlock(&context->mutex_write);
-            return RBUF_TIMEOUT;
-        }
+  while (1) {
+    if (space_needed > total_space) {
+      pthread_mutex_unlock(&context->mutex_write);
+      return RINGBUFFER_FULL;
     }
 
-    // Write message length
-    if (context->write + sizeof(size_t) <= context->end) {
-        memcpy(context->write, &message_len, sizeof(size_t));
-        context->write += sizeof(size_t);
+    size_t available_space;
+    if (context->write >= context->read) {
+      available_space = (context->end - context->write) +
+                        (context->read - context->begin) - 1;
     } else {
-        size_t first_part_size = context->end - context->write;
-        memcpy(context->write, &message_len, first_part_size);
-        memcpy(context->begin, (uint8_t *)&message_len + first_part_size, sizeof(size_t) - first_part_size);
-        context->write = context->begin + (sizeof(size_t) - first_part_size);
+      available_space = context->read - context->write - 1;
     }
 
-    // Write message content
-    if (context->write + message_len <= context->end) {
-        memcpy(context->write, message, message_len);
-        context->write += message_len;
-    } else {
-        size_t first_part_size = context->end - context->write;
-        memcpy(context->write, message, first_part_size);
-        memcpy(context->begin, (uint8_t *)message + first_part_size, message_len - first_part_size);
-        context->write = context->begin + (message_len - first_part_size);
+    if (available_space >= space_needed) {
+      break;
     }
 
-    pthread_cond_signal(&context->signal_read);
-    pthread_mutex_unlock(&context->mutex_write);
+    int res = pthread_cond_timedwait(&context->signal_write,
+                                     &context->mutex_write, &timeout);
+    if (res == ETIMEDOUT) {
+      pthread_mutex_unlock(&context->mutex_write);
+      return RBUF_TIMEOUT;
+    }
+  }
 
-    return SUCCESS;
+  // Write message length
+  if (context->write + sizeof(size_t) <= context->end) {
+    memcpy(context->write, &message_len, sizeof(size_t));
+    context->write += sizeof(size_t);
+  } else {
+    size_t first_part_size = context->end - context->write;
+    memcpy(context->write, &message_len, first_part_size);
+    memcpy(context->begin, (uint8_t *)&message_len + first_part_size,
+           sizeof(size_t) - first_part_size);
+    context->write = context->begin + (sizeof(size_t) - first_part_size);
+  }
+
+  // Write message content
+  if (context->write + message_len <= context->end) {
+    memcpy(context->write, message, message_len);
+    context->write += message_len;
+  } else {
+    size_t first_part_size = context->end - context->write;
+    memcpy(context->write, message, first_part_size);
+    memcpy(context->begin, (uint8_t *)message + first_part_size,
+           message_len - first_part_size);
+    context->write = context->begin + (message_len - first_part_size);
+  }
+
+  pthread_cond_signal(&context->signal_read);
+  pthread_mutex_unlock(&context->mutex_write);
+
+  return SUCCESS;
 }
 
 size_t read_message_size_from_ringbuffer(rbctx_t *context) {
-    size_t value;
-    uint8_t *read_ptr = context->read;
-    size_t size_t_size = sizeof(size_t);
+  size_t value;
+  uint8_t *read_ptr = context->read;
+  size_t size_t_size = sizeof(size_t);
 
-    if (read_ptr + size_t_size <= context->end) {
-        memcpy(&value, read_ptr, size_t_size);
-    } else {
-        size_t first_part_size = context->end - read_ptr;
-        uint8_t temp_buffer[size_t_size];
+  if (read_ptr + size_t_size <= context->end) {
+    memcpy(&value, read_ptr, size_t_size);
+  } else {
+    size_t first_part_size = context->end - read_ptr;
+    uint8_t temp_buffer[size_t_size];
 
-        memcpy(temp_buffer, read_ptr, first_part_size);
-        memcpy(temp_buffer + first_part_size, context->begin, size_t_size - first_part_size);
-        memcpy(&value, temp_buffer, size_t_size);
-    }
+    memcpy(temp_buffer, read_ptr, first_part_size);
+    memcpy(temp_buffer + first_part_size, context->begin,
+           size_t_size - first_part_size);
+    memcpy(&value, temp_buffer, size_t_size);
+  }
 
-    context->read += size_t_size;
-    if (context->read >= context->end) {
-        context->read = context->begin + (context->read - context->end);
-    }
+  context->read += size_t_size;
+  if (context->read >= context->end) {
+    context->read = context->begin + (context->read - context->end);
+  }
 
-    return value;
+  return value;
 }
 
 int ringbuffer_read(rbctx_t *context, void *buffer, size_t *buffer_len) {
-    struct timespec timeout;
-    clock_gettime(CLOCK_REALTIME, &timeout);
-    timeout.tv_sec += 1;
+  struct timespec timeout;
+  clock_gettime(CLOCK_REALTIME, &timeout);
+  timeout.tv_sec += 1;
 
-    pthread_mutex_lock(&context->mutex_read);
+  pthread_mutex_lock(&context->mutex_read);
 
-    while (context->read == context->write) {
-        int res = pthread_cond_timedwait(&context->signal_read, &context->mutex_read, &timeout);
-        if (res == ETIMEDOUT) {
-            pthread_mutex_unlock(&context->mutex_read);
-            return RINGBUFFER_EMPTY;
-        }
+  while (context->read == context->write) {
+    int res = pthread_cond_timedwait(&context->signal_read,
+                                     &context->mutex_read, &timeout);
+    if (res == ETIMEDOUT) {
+      pthread_mutex_unlock(&context->mutex_read);
+      return RINGBUFFER_EMPTY;
     }
+  }
 
-    size_t size = read_message_size_from_ringbuffer(context);
+  size_t size = read_message_size_from_ringbuffer(context);
 
-    if (*buffer_len < size) {
-        pthread_mutex_unlock(&context->mutex_read);
-        return -1; // Buffer too small
-    }
-
-    if (context->read + size <= context->end) {
-        memcpy(buffer, context->read, size);
-        context->read += size;
-    } else {
-        size_t first_part = context->end - context->read;
-        size_t second_part = size - first_part;
-        memcpy(buffer, context->read, first_part);
-        memcpy((uint8_t *)buffer + first_part, context->begin, second_part);
-        context->read = context->begin + second_part;
-    }
-
-    if (context->read == context->end) {
-        context->read = context->begin;
-    }
-
-    pthread_cond_signal(&context->signal_write);
+  if (*buffer_len < size) {
     pthread_mutex_unlock(&context->mutex_read);
+    return -1; // Buffer too small
+  }
 
-    *buffer_len = size;
-    return SUCCESS;
+  if (context->read + size <= context->end) {
+    memcpy(buffer, context->read, size);
+    context->read += size;
+  } else {
+    size_t first_part = context->end - context->read;
+    size_t second_part = size - first_part;
+    memcpy(buffer, context->read, first_part);
+    memcpy((uint8_t *)buffer + first_part, context->begin, second_part);
+    context->read = context->begin + second_part;
+  }
+
+  if (context->read == context->end) {
+    context->read = context->begin;
+  }
+
+  pthread_cond_signal(&context->signal_write);
+  pthread_mutex_unlock(&context->mutex_read);
+
+  *buffer_len = size;
+  return SUCCESS;
 }
 void ringbuffer_destroy(rbctx_t *context) {
-    pthread_mutex_destroy(&context->mutex_read);
-    pthread_mutex_destroy(&context->mutex_write);
-    pthread_cond_destroy(&context->signal_write);
-    pthread_cond_destroy(&context->signal_read);
+  pthread_mutex_destroy(&context->mutex_read);
+  pthread_mutex_destroy(&context->mutex_write);
+  pthread_cond_destroy(&context->signal_write);
+  pthread_cond_destroy(&context->signal_read);
 
-    context->begin = NULL;
-    context->end = NULL;
-    context->write = NULL;
-    context->read = NULL;
+  context->begin = NULL;
+  context->end = NULL;
+  context->write = NULL;
+  context->read = NULL;
 }
